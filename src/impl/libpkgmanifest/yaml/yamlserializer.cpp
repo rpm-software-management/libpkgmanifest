dc071214979a633d3a46094834d125133668ff0f
Jan Kolarik <jkolarik@redhat.com>
2024-09-03 14:58:58 +0200

    yamlserializer: Throw an exception on non-existing file path

--- src/impl/libpkgmanifest/yaml/yamlserializer.cpp
+++ src/impl/libpkgmanifest/yaml/yamlserializer.cpp
@@ -14,8 +14,10 @@ std::string YamlSerializer::to_string(const IYamlNode & node) const {
 }
 
 void YamlSerializer::to_file(const IYamlNode & node, const std::string & path) const {
-    std::ofstream file_stream(path);
+    std::ofstream file_stream;
+    file_stream.exceptions(std::ofstream::badbit | std::ofstream::failbit);
     try {
+        file_stream.open(path);
         file_stream << dynamic_cast<const YamlNode *>(&node)->get_node();
     } catch (std::ofstream::failure & ex) {
         throw FileOutputError(ex.what());
